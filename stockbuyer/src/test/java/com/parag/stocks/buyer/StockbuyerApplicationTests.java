5482d96f6baea9974847bf53433301ef78b9cca5
Parag Parate <paragparate@gmail.com>
2018-12-31 09:41:06 +0530

    moved files to stockbuyer project

---
+++ stockbuyer/src/test/java/com/parag/stocks/buyer/StockbuyerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.parag.stocks.buyer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockbuyerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
